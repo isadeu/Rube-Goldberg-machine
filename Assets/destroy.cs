dd8b617a9037500ea011e5a523ceca5d8cc48cae
Isa Deu <ipd220@nyu.edu>
2018-02-11 22:33:44 -0500

    pre 1

---
+++ Assets/destroy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroy : MonoBehaviour {
+
+	public GameObject bowl;
+	public MeshCollider bomesh;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerStay (Collider c) {
+
+
+		Rigidbody rb = bowl.AddComponent(typeof(Rigidbody)) as Rigidbody;
+		//FixedJoint jnt = bowl.AddComponent(typeof(FixedJoint)) as FixedJoint;  dont do this it makes evrything slow and sad
+		Destroy (bomesh);
+		//GameObject myGameObject = new GameObject("Test Object");
+		//Rigidbody gameObjectsRigidBody = bowl.AddComponent<Rigidbody>;
+
+
+	
+	}
+}
