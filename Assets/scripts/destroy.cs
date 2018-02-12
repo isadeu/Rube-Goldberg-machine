3f0e55c46204769240af2a0f19eb588ef13e36a7
Isa Deu <ipd220@nyu.edu>
2018-02-12 01:32:32 -0500

    comença a destroçar-ho tot

---
+++ Assets/scripts/destroy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroy : MonoBehaviour {
+
+	public GameObject bowl;
+	public MeshCollider bomesh;
+
+	public Camera camera1;
+	public Camera camera2;
+
+	/*var Camera1 Camera;
+	var Camera2 Camera;*/
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerStay (Collider c) {
+
+
+		Rigidbody rb = bowl.AddComponent(typeof(Rigidbody)) as Rigidbody;
+		//FixedJoint jnt = bowl.AddComponent(typeof(FixedJoint)) as FixedJoint;  dont do this it makes evrything slow and sad
+		Destroy (bomesh);
+		//GameObject myGameObject = new GameObject("Test Object");
+		//Rigidbody gameObjectsRigidBody = bowl.AddComponent<Rigidbody>;
+
+		camera1.enabled = false;
+		camera2.enabled = true; 
+
+	
+	}
+}
